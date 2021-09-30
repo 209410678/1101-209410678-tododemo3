c52de4bdce76076eccc34049fdce7c347a08f364
209410678 <li201217n@gmail.com>
2021-09-30 21:00:44 +0800

    first commit

---
+++ src/App_78.js
@@ -0,0 +1,15 @@
+import Todo_78 from './components/Todo_78';
+import './App.css';
+
+function App_78() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_78 text="Learn React" />
+      <Todo_78 text="SQL" />
+      <Todo_78 text="NextJS" />
+    </div>
+  );
+}
+
+export default App_78;
