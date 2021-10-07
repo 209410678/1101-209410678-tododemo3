f5a19a230b626f061360da1313653487cc572655
209410678 <209410678@gms.tku.edu.tw>
2021-10-07 19:53:00 +0800

    add <Modal_78 /> <Backdrop_78 /> components

--- src/components/Todo_78.js
+++ src/components/Todo_78.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
+import Modal_78 from './Modal_78';
+import Backdrop_78 from './Backdrop_78'
 
 function Todo_78(props) {
+  const [showModal,setShowModal] = useState();
+
+  function showModalHandler(){
+    setShowModal(true);
+  }
+
+  function closeModalHandler(){
+    setShowModal(false);
+  }
+
   return (
     <div>
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">Delete</button>
+          <button className="btn" onClick={showModalHandler}>Delete</button>
         </div>
       </div>
+      {showModal && <Backdrop_78 onClose={closeModalHandler}/>}
+      {showModal && <Modal_78 text= 'Are you sure?' onClose={closeModalHandler}/>}
     </div>
   );
 }
